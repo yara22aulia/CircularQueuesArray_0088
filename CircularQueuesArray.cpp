c5f87639ebfee02c4bc9158706b2a11d6ccc63e7
yara22aulia <yara22aulia@gmail.com>
2026-05-28 18:39:43 +0700

    memperbarui posisi front setelah melakukan delete

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -56,11 +56,20 @@ public:
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
-               // cek jika antrian hanya memiliki satu elemen
+        // cek jika antrian hanya memiliki satu elemen
         if (FRONT == REAR)
         {
             FRONT = -1;
             REAR = -1;
         }
+                else
+        {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }    
 
 };
