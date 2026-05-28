0d29ffdd3b0bce885a408c593a9efef2b635ab95
yara22aulia <yara22aulia@gmail.com>
2026-05-28 18:15:39 +0700

    menyimpan data ke dalam queue

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -35,6 +35,14 @@ public:
             FRONT = 0;
             REAR = 0;
         }
+           else
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
     }
 
 };
