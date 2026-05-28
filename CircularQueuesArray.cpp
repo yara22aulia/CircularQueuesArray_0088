a77ac1a68863a1a53dcca884160f7a4df9e40f15
yara22aulia <yara22aulia@gmail.com>
2026-05-28 18:35:29 +0700

    mengatur front dan rear kalo queue menjadi kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -55,5 +55,12 @@ public:
             return;
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
-        
+
+               // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+
 };
